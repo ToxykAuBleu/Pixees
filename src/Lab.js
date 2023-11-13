beb96cabd4426c2539da1fef1523261f4de04caa
Mattis Pingard <mattis.pingard@gmail.com>
2023-11-13 16:03:17 +0100

    [+] Création de la classe Lab

---
+++ src/Lab.js
@@ -0,0 +1,11 @@
+import { Couleur } from './Couleur.js';
+
+export class Lab extends Couleur {
+    constructor(comp1, comp2, comp3) {
+        super(comp1, comp2, comp3);
+    }
+
+    calculDeltaE(labComp) {
+
+    }
+}
