69552c88068a958da234bf7871c109c084dba29c
Adrien Hiribarren Touya <adrien64250@yahoo.com>
2023-11-15 10:07:16 +0100

    [+] Ajout des getters et setters pour hauteur et largeur [+] Ajout des commentaires pour la documentation [~] Modification du nom de la liste de pixels (_image devient _pixels) [~] Modification du constructeur pour intialiser la liste de pixels

--- src/Grille.js
+++ src/Grille.js
@@ -1,20 +1,70 @@
 import { Pixel } from "./Pixel.js";
 import { Coordonnees } from "./Coordonnees.js";
-
+/**
+ * Classe représentant une Grille.
+ */
 export class Grille {
+    // ATTRIBUTS
+    /**
+     * Hauteur de la Grille.
+     */
     #_hauteur;
+    /**
+     * Largeur de la Grille.
+     */
     #_largeur;
-    #_image;
+    /**
+     * Liste de Pixels de la Grille.
+     */
+    #_pixels;
 
-    constructor(h, l) {
+    // CONSTRUCTEUR
+    /**
+     * Crée une nouvelle Grille avec (par défaut) une hauteur, une largeur et une liste de Pixels.
+     * @param {int} h La hauteur de la Grille.
+     * @param {int} l La largeur de la Grille.
+     * @param {Array<Pixel>} pixels La liste de Pixels de la Grille. 
+     */
+    constructor(h, l, pixels = []) {
         this.#_hauteur = h;
         this.#_largeur = l;
+        this.#_pixels = pixels;
     }
 
+    // GETTERS & SETTERS
+    /** 
+     * Retourne la hauteur de la Grille.
+     * @returns {int} La hauteur de la Grille.
+     */
+    getHauteur() { return this.#_hauteur; }
+    /**
+     * Retourne la largeur de la Grille.
+     * @returns {int} La largeur de la Grille.
+     */
+    getLargeur() { return this.#_largeur; }
+
+    /**
+     * Définit la hauteur de la Grille.
+     * @param {int} h La nouvelle hauteur de la Grille.
+     */
+    setHauteur(h) { this.#_hauteur = h; }
+    /**
+     * Définit la largeur de la Grille.
+     * @param {int} l La nouvelle largeur de la Grille.
+     */
+    setLargeur(l) { this.#_largeur = l; }
+
+    // MÉTHODES
+    /**
+     * Retourne le Pixel à la position (x, y) de la Grille.
+     * @param {int} x La position en x du Pixel.
+     * @param {int} y La position en y du Pixel.
+     * @returns {Pixel} Le Pixel à la position (x, y) de la Grille.
+     */
     getPixelAt(x, y) {
         let coordPixelCherche = new Coordonnees(x, y);
 
-        image.forEach(pixel => {
+        this.#_pixels.forEach(pixel => {
             if (pixel.getCoord() == coordPixelCherche) {
                 return pixel;
             }
@@ -23,14 +73,23 @@ export class Grille {
         return null;
     }
 
+    /**
+     * Ajoute un Pixel à la liste de Pixels "pixels"
+     * @param {Pixel} unPixel
+     */
     ajouterPixel(unPixel) {
-        image.push(unPixel);
+        this.#_pixels.push(unPixel);
     }
 
+    /**
+     * Retire un Pixel de la liste de Pixels "pixels"
+     * @param {Pixel} unPixel
+     * @returns {boolean} true si le Pixel a été retiré, false sinon.
+     */
     retirerPixel(unPixel) {
-        image.forEach(pixel => {
+        this.#_pixels.forEach(pixel => {
             if (pixel == unPixel) {
-                image.splice(image.indexOf(pixel), 1);
+                this.#_pixels.splice(this.#_pixels.indexOf(pixel), 1);
                 return true;
             }
         });
