975d1546e402b83bc746a11231378ee0e4e0dcf8
neoelduayen <nelduayen@univ-pau.fr>
2023-11-15 16:31:33 +0100

    Ajout d'un script de test pour afficher la grille avec des carrés de 20px

---
+++ src/test.js
@@ -0,0 +1,36 @@
+import { Grille } from "./Grille.js";
+import { Pixel } from "./Pixel.js";
+import { Coordonnees } from "./Coordonnees.js";
+
+// TEST - Grille avec une liste de Pixel.
+let grilleListe = new Grille(128, 128);
+
+// Créer la grille dans le HTML
+let grilleContainer = document.createElement('div');
+grilleContainer.id = 'grilleListe';
+document.body.appendChild(grilleContainer);
+
+// Initialisation de la grille.
+for (let x = 0; x < grilleListe.getHauteur(); x++) {
+    for (let y = 0; y < grilleListe.getLargeur(); y++) {
+        let pixel = new Pixel();
+        pixel.setCoord(new Coordonnees(x, y));
+        grilleListe.ajouterPixel(pixel);
+    }
+}
+console.log(grilleListe);
+
+// Ajouter les divs des pixels à la grille dans le HTML
+for (let x = 0; x < grilleListe.getHauteur(); x++) {
+    for (let y = 0; y < grilleListe.getLargeur(); y++) {
+        // Créer un élément div pour chaque pixel
+        let divPixel = document.createElement('div');
+
+        // Définir les coordonnées du pixel comme une classe pour le style CSS si nécessaire
+        divPixel.classList.add('pixel');
+        divPixel.classList.add('coord-' + x + '-' + y);
+
+        // Ajouter le div à la grille dans le HTML
+        grilleContainer.appendChild(divPixel);
+    }
+}
