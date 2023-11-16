6b54b99da6a1cc0f9618594f89cd6f10ff2edbb8
ToxykAuBleu <foissac.mathieu@gmail.com>
2023-11-16 09:02:51 +0100

    [+] Ajout des temps que prend les opérations: init, getPixelAt.

--- src/test.js
+++ src/test.js
@@ -3,9 +3,10 @@ import { Pixel } from "./Pixel.js";
 import { Coordonnees } from "./Coordonnees.js";
 
 // TEST - Grille avec une liste de Pixel.
-let grilleListe = new Grille(128, 128);
+let grilleListe = new Grille(1000, 1000);
 
 // Initialisation de la grille.
+let started = new Date();
 for (let x = 0; x < grilleListe.getHauteur(); x++) {
     for (let y = 0; y < grilleListe.getLargeur(); y++) {
         let pixel = new Pixel();
@@ -13,4 +14,18 @@ for (let x = 0; x < grilleListe.getHauteur(); x++) {
         grilleListe.ajouterPixel(pixel);
     }
 }
-console.log(grilleListe);
+let finished = new Date() - started; // Calcul du temps qu'a duré l'operation
+console.log(`grilleListe | Initialisation: ${finished} ms`, grilleListe);
+
+// Récupérer un Pixel dans la grille en 0 0, en plein milieu et tout en bas à droite.
+const calcGetTime = (x, y) => {
+    started = new Date();
+    let pixel = grilleListe.getPixelAt(x, y);
+    finished = new Date() - started; // Calcul du temps qu'a duré l'operation
+    console.log(`grilleListe | Récupération en ${x}, ${y}: ${finished} ms`, pixel);
+}
+const l = grilleListe.getLargeur();
+const h = grilleListe.getHauteur();
+calcGetTime(0,0);
+calcGetTime((l/2)-1,(h/2)-1);
+calcGetTime(l-1,h-1);
