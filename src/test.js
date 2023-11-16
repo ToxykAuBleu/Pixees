dd84a9ba103c5cd76ce3375c0c377b5711727515
Mattis Pingard <mattis.pingard@gmail.com>
2023-11-16 11:25:12 +0100

    [~] Fix : suppression et création de la grille lors de l'importation de l'image [+] Création de fonction

--- src/test.js
+++ src/test.js
@@ -1,29 +1,39 @@
 import { Grille } from "./Grille.js";
+import { RGB } from "./RGB.js";
+
+const canvas = document.getElementsByTagName("canvas")[0];
+const ctx = canvas.getContext("2d", { willReadFrequently: true });
+let grilleMain = drawMainCanvas(16, 16);
 
 // Création du pattern
-const patternCanvas = document.createElement("canvas");
-const patternContext = patternCanvas.getContext("2d");
+function drawPattern() {
+    const patternCanvas = document.createElement("canvas");
+    const patternContext = patternCanvas.getContext("2d");
 
-patternCanvas.width = 2;
-patternCanvas.height = 2;
+    patternCanvas.width = 2;
+    patternCanvas.height = 2;
 
-patternContext.fillStyle = "#ddd";
-patternContext.fillRect(0, 0, 1, 1);
-patternContext.fillRect(1, 1, 1, 1);
-patternContext.stroke();
+    patternContext.fillStyle = "#ddd";
+    patternContext.fillRect(0, 0, 1, 1);
+    patternContext.fillRect(1, 1, 1, 1);
+    patternContext.stroke();
 
-let grille = new Grille(16, 16);
+    return patternCanvas;
+}
 
-let canvas = document.getElementsByTagName("canvas")[0];
+function drawMainCanvas(hauteur, largeur) {
+    const grille = new Grille(hauteur, largeur)
 
-canvas.width = grille.getLargeur();
-canvas.height = grille.getHauteur();
+    canvas.width = grille.getLargeur();
+    canvas.height = grille.getHauteur();
 
-// Ajout du pattern sur le canvas de base
-const ctx = canvas.getContext("2d");
-const pattern = ctx.createPattern(patternCanvas, "repeat");
-ctx.fillStyle = pattern;
-ctx.fillRect(0, 0, grille.getLargeur(), grille.getHauteur());
+    // Ajout du pattern sur le canvas de base
+    const pattern = ctx.createPattern(drawPattern(), "repeat");
+    ctx.fillStyle = pattern;
+    ctx.fillRect(0, 0, grille.getLargeur(), grille.getHauteur());
+
+    return grille;
+}
 
 // Affichage de l'image sur le canvas
 const inputElement = document.getElementById("imageToLoad");
@@ -31,11 +41,20 @@ inputElement.addEventListener("change", handleImage, false);
 function handleImage() {
     const image = new Image();
     image.src = URL.createObjectURL(this.files[0]);
-    image.onload = function() {
-        grille.setHauteur(image.height);
-        grille.setLargeur(image.width);
-        canvas.width = grille.getLargeur();
-        canvas.height = grille.getHauteur();
+    image.onload = function () {
+        grilleMain = drawMainCanvas(image.height, image.width);
+        canvas.width = grilleMain.getLargeur();
+        canvas.height = grilleMain.getHauteur();
         ctx.drawImage(image, 0, 0);
+        getImageData();
     }
+}
+
+function getImageData() {
+    let data = ctx.getImageData(0, 0, grilleMain.getLargeur(), grilleMain.getHauteur()).data;
+    for (let i = 0; i < data.length; i += 4) {
+        let pixel = new RGB(data[i], data[i + 1], data[i + 2], data[i + 3]);
+        grilleMain.ajouterPixel(pixel);
+    }
+    console.log(grilleMain);
 }
