2c73fdf7bb8f51e162669f98ae47f5690d0201e8
Mattis Pingard <mattis.pingard@gmail.com>
2023-11-17 10:29:26 +0100

    [+] Ajout des commentaires

--- src/test.js
+++ src/test.js
@@ -9,12 +9,13 @@ let grilleMain = drawMainCanvas(16, 16);
 
 // Création du pattern
 function drawPattern() {
+    // Création du Canvas pour dessiner le pattern
     const patternCanvas = document.createElement("canvas");
     const patternContext = patternCanvas.getContext("2d");
-
     patternCanvas.width = 2;
     patternCanvas.height = 2;
 
+    // Dessin du pattern
     patternContext.fillStyle = "#ddd";
     patternContext.fillRect(0, 0, 1, 1);
     patternContext.fillRect(1, 1, 1, 1);
@@ -23,16 +24,18 @@ function drawPattern() {
     return patternCanvas;
 }
 
+// Création du Canvas principal
 function drawMainCanvas(hauteur, largeur) {
     const grille = new Grille(hauteur, largeur)
 
-    canvas.width = grille.getLargeur();
-    canvas.height = grille.getHauteur();
+    // Ajustement de la taille du Canvas
+    canvas.width = largeur;
+    canvas.height = hauteur;
 
     // Ajout du pattern sur le canvas de base
     const pattern = ctx.createPattern(drawPattern(), "repeat");
     ctx.fillStyle = pattern;
-    ctx.fillRect(0, 0, grille.getLargeur(), grille.getHauteur());
+    ctx.fillRect(0, 0, largeur, hauteur);
 
     return grille;
 }
@@ -40,10 +43,14 @@ function drawMainCanvas(hauteur, largeur) {
 // Affichage de l'image sur le canvas
 const inputElement = document.getElementById("imageToLoad");
 inputElement.addEventListener("change", handleImage, false);
+
+// Gestion de l'affichage de l'image
 function handleImage() {
+    // Création et stockage de l'Image
     const image = new Image();
     image.src = URL.createObjectURL(this.files[0]);
     image.onload = function () {
+        // Dessin et redimension du Canvas
         grilleMain = drawMainCanvas(image.height, image.width);
         canvas.width = grilleMain.getLargeur();
         canvas.height = grilleMain.getHauteur();
@@ -52,16 +59,24 @@ function handleImage() {
     }
 }
 
+// Récupération des données de l'image pour les mettre dans la grille de pixel
 function getImageData() {
     let ligne = 0;
     let colonne = 0;
+    // On récupère les données du Canvas
     let data = ctx.getImageData(0, 0, grilleMain.getLargeur(), grilleMain.getHauteur()).data;
+    // On parcours la liste de données
     for (let i = 0; i < data.length; i += 4) {
+        // Création de la Couleur sous forme RGB
         let rgba = new RGB(data[i], data[i + 1], data[i + 2], data[i + 3]);
+        // Création d'un nouveau Pixel
         let pixel = new Pixel(false);
+        // On ajoute la couleur au Pixel
         pixel.setColor(rgba);
+        // On place le pixel dans la Grille
         grilleMain.setPixelAt(colonne, ligne, pixel);
         colonne++;
+        // Si on atteint la dernière colonne, on change de ligne et on revient à la première colonne
         if (colonne == grilleMain.getLargeur()) {
             colonne = 0;
             ligne++;
@@ -70,14 +85,18 @@ function getImageData() {
     isImageLoaded = true;
 }
 
+// Récuperer la position du curseur
 function getMousePosition(event) {
+    // On calcule le ratio pour récupérer le pixel selon la taille du Canvas et la taille de l'image
     let rect = canvas.getBoundingClientRect();
     let scaleX = canvas.width / rect.width;
     let scaleY = canvas.height / rect.height
 
+    // On récupère la position exacte du Pixel
     let x = Math.floor((event.clientX - rect.left) * scaleX);
     let y = Math.floor((event.clientY - rect.top) * scaleY);
-    
+
+    // On met a jour l'interface avec les données
     document.getElementById("x").innerText = x;
     document.getElementById("y").innerText = y;
     if (isImageLoaded) {
@@ -86,23 +105,27 @@ function getMousePosition(event) {
     }
 }
 
+// On écoute les mouvements de souris
 canvas.addEventListener("mousemove", function(e) {
     getMousePosition(e);
 });
 
 var slider = document.getElementById("tolerance");
 var display = document.getElementById("toleranceValue");
-display.value = slider.value; // Display the default slider value
+// affichage de la valeur par défaut du slider
+display.value = slider.value;
 
-// Update the current slider value (each time you drag the slider handle)
+// Mise à jour de la text box quand on change la valeur du slider
 slider.oninput = function() {
     display.value = this.value;
 }
 
+// Mise à jour du slider quand on change la valeur dans le text box 
 display.oninput = function() {
     slider.value = this.value;
 }
 
+// Pour rester dans un pourcentage
 display.onchange = function() {
     if (Number(this.value) > 100) {
         this.value = 100;
