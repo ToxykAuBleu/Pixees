9c887f8e9b3f771158591ec1e3b525ced5c8210e
Mattis Pingard <mattis.pingard@gmail.com>
2023-11-17 15:35:55 +0100

    [+] Ajout de la requête 8

--- bdd/selection.sql
+++ bdd/selection.sql
@@ -1,15 +1,23 @@
-# Requête 1
+-- 1. Afficher tous les projets d'un utilisateur
 SELECT * FROM Projet P
 JOIN Editer E ON P.idProjet = E.idProjet;
 
-# Requête 4
+-- 2. Afficher toutes les publications d'un utilisateur.
+SELECT * FROM Publication
+WHERE idUtilisateur = 3;
+
+-- 4. Afficher le nombre total de publication
 SELECT COUNT(idProjet) FROM Editer
 WHERE idUtilisateur = 1;
 
-# Requête 5
+-- 5. Afficher le nombre total de like
 SELECT SUM(idUtilisateur) FROM AimerPub
 WHERE idPublication = 1;
 
-# Requête 7
+-- 7. Afficher le nombre de personne suivis
 SELECT SUM(idUtilisateur_1) FROM SuivreArtiste
 WHERE idUtilisateur = 1;
+
+-- 8. Afficher le nombre de commentaire sur une publication
+SELECT COUNT(idCommentaire) FROM Commentaire
+WHERE idPublication = 1;
