24f30b2cda34ca604eac3fbc6864d34c98c172b4
ToxykAuBleu <foissac.mathieu@gmail.com>
2023-11-17 15:10:46 +0100

    [+] Création de la requête de sélection n°3.

--- bdd/selection.sql
+++ bdd/selection.sql
@@ -1,3 +1,9 @@
 -- 2. Afficher toutes les publications d'un utilisateur.
 SELECT * FROM Publication
 WHERE idUtilisateur = 3;
+
+-- 3. Afficher tous les signalements réalisés par un utilisateur.
+SELECT idSignalement, idUtilisateur Auteur, dateSignalement, idRaison,
+ idCommentaire CommentaireSignalé, idPublication PublicationSignalé, idUtilisateur_1 ProfilSignalé
+FROM Signalement
+WHERE idUtilisateur = 3;
