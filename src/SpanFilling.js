7ed600f88afefd4c7cfa5c7e04a94be64282abdd
ToxykAuBleu <foissac.mathieu@gmail.com>
2023-12-18 15:04:11 +0100

    [+] Continuation de l'implémentation de l'algo de SpanFilling.

--- src/SpanFilling.js
+++ src/SpanFilling.js
@@ -1,6 +1,7 @@
 import { Couleur } from "./Couleur";
 import { Pixel } from "./Pixel";
 import { Calque } from "./Calque";
+import { Coordonnees } from "./Coordonnees";
 
 /**
  * Applique l'algorithme de SpanFilling sur le calque/grille.
@@ -8,11 +9,47 @@ import { Calque } from "./Calque";
  * @param {Calque} calque Représente le calque sur lequel on travaille.
  * @param {Number} coordX Représente la coordonnée X du pixel d'origine.
  * @param {Number} coordY Représente la coordonnée Y du pixel d'origine.
- * @param {Array} fileTraitement Représente la file de traitement contenant des Pixel.
+ * @param {Array<Coordonnees>} fileTraitement Représente la file de traitement contenant des Coordonnées des Pixels.
  * @param {Pixel} pixelOrigine Représente le Pixel d'origine (pixel sélectionné par l'utilisateur).
  */
 function spanFilling(tolerance, calque, coordX, coordY, fileTraitement, pixelOrigine) {
-    
+    while (true) {
+        // Vérification conditions d'arrêt.
+        if (fileTraitement.length === 0) {
+            break;
+        }
+
+        // tolerance, calque, coordX, coordY, fileTraitement, pixelOrigine
+        // >> Traitement d'une ligne de pixels. >> calque
+
+        // fileTraitement >> Récupération du pixel à traiter >> coordsTraitement, partieGaucheX, partieDroiteX, yFixe
+        let coordsTraitement = fileTraitement.shift();
+        let yFixe = coordsTraitement.getY();
+        let partieGaucheX = coordsTraitement.getX();
+        let partieDroiteX = coordsTraitement.getX();
+
+        // Traitement de la partie gauche du pixel courant. >> partieGaucheX, calque
+        while (checkIfInside(calque, partieGaucheX - 1, yFixe) 
+        && checkTolerance(calque, tolerance, partieGaucheX - 1, yFixe, pixelOrigine)) 
+        {
+            // Sélection du pixel courant, puis pixel suivant.
+            calque.getGrille().getPixelAt(partieGaucheX - 1, yFixe).setSelected(true);
+            partieGaucheX--;
+        }
+
+        // Traitement de la partie droite du pixel courant. >> partieDroiteX, calque
+        while (checkIfInside(calque, partieDroiteX, yFixe) 
+        && checkTolerance(calque, tolerance, partieDroiteX, yFixe, pixelOrigine)) 
+        {
+            // Sélection du pixel courant, puis pixel suivant.
+            calque.getGrille().getPixelAt(partieDroiteX, yFixe).setSelected(true);
+            partieDroiteX++;
+        }
+
+        // partieGaucheX, partieDroiteX, yFixe, fileTraitement >> Scan dans les lignes du dessus et du dessous >> fileTraitement
+        scanLine(partieGaucheX, partieDroiteX - 1, yFixe + 1, fileTraitement)
+        scanLine(partieGaucheX, partieDroiteX - 1, yFixe - 1, fileTraitement)
+    }
 }
 
 export { spanFilling };
