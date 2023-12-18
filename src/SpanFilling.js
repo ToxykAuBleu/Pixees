4a1da3af505bebc3314aa433942470d0d5420406
ToxykAuBleu <foissac.mathieu@gmail.com>
2023-12-18 15:10:39 +0100

    [-] Suppression de coordX et coordY (inutilisé).

--- src/SpanFilling.js
+++ src/SpanFilling.js
@@ -1,4 +1,3 @@
-import { Couleur } from "./Couleur";
 import { Pixel } from "./Pixel";
 import { Calque } from "./Calque";
 import { Coordonnees } from "./Coordonnees";
@@ -7,19 +6,17 @@ import { Coordonnees } from "./Coordonnees";
  * Applique l'algorithme de SpanFilling sur le calque/grille.
  * @param {Float32Array} tolerance Représente la tolérance de remplissage.
  * @param {Calque} calque Représente le calque sur lequel on travaille.
- * @param {Number} coordX Représente la coordonnée X du pixel d'origine.
- * @param {Number} coordY Représente la coordonnée Y du pixel d'origine.
  * @param {Array<Coordonnees>} fileTraitement Représente la file de traitement contenant des Coordonnées des Pixels.
  * @param {Pixel} pixelOrigine Représente le Pixel d'origine (pixel sélectionné par l'utilisateur).
  */
-function spanFilling(tolerance, calque, coordX, coordY, fileTraitement, pixelOrigine) {
+function spanFilling(tolerance, calque, fileTraitement, pixelOrigine) {
     while (true) {
         // Vérification conditions d'arrêt.
         if (fileTraitement.length === 0) {
             break;
         }
 
-        // tolerance, calque, coordX, coordY, fileTraitement, pixelOrigine
+        // tolerance, calque, fileTraitement, pixelOrigine
         // >> Traitement d'une ligne de pixels. >> calque
 
         // fileTraitement >> Récupération du pixel à traiter >> coordsTraitement, partieGaucheX, partieDroiteX, yFixe
