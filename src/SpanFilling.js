1f21ba50a1265ada9c57a09a65a4f75f3e3273b4
ToxykAuBleu <foissac.mathieu@gmail.com>
2023-12-18 14:29:03 +0100

    [+] Début de l'implémentation de l'algorithme de SpanFilling.

---
+++ src/SpanFilling.js
@@ -0,0 +1,18 @@
+import { Couleur } from "./Couleur";
+import { Pixel } from "./Pixel";
+import { Calque } from "./Calque";
+
+/**
+ * Applique l'algorithme de SpanFilling sur le calque/grille.
+ * @param {Float32Array} tolerance Représente la tolérance de remplissage.
+ * @param {Calque} calque Représente le calque sur lequel on travaille.
+ * @param {Number} coordX Représente la coordonnée X du pixel d'origine.
+ * @param {Number} coordY Représente la coordonnée Y du pixel d'origine.
+ * @param {Array} fileTraitement Représente la file de traitement contenant des Pixel.
+ * @param {Pixel} pixelOrigine Représente le Pixel d'origine (pixel sélectionné par l'utilisateur).
+ */
+function spanFilling(tolerance, calque, coordX, coordY, fileTraitement, pixelOrigine) {
+    
+}
+
+export { spanFilling };
