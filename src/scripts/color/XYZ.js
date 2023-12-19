9a38b62d60b184e08683948f9c8385368b3fdbb0
ToxykAuBleu <foissac.mathieu@gmail.com>
2023-12-19 16:59:40 +0100

    [~] Modification des calculs de L*a*b*.

--- src/scripts/color/XYZ.js
+++ src/scripts/color/XYZ.js
@@ -6,15 +6,16 @@ import { Lab } from './Lab.js';
 const Xn = 95.04;
 const Yn = 100.0;
 const Zn = 108.88;
-const delta = 6.0 / 29.0;
+const epsilon = 216.00000000000000 / 24389.00000000000000;
+const kappa = 24389.00000000000000 / 27.00000000000000;
 
 // Fonction f utilisée dans la conversion XYZ vers Lab
 
 function f(t) {
-    if (t > delta**3) {
+    if (t > epsilon) {
         return Math.cbrt(t);
     } else {
-        return (7.787 * t) + (16 / 116);
+        return (kappa * t + 16) / 116;
     }
 }
 
@@ -39,13 +40,13 @@ export class XYZ extends Couleur {
      * @returns {Lab} La Couleur sous forme Lab
      */
     XYZversLab() {
-        const X=this.getComp(1) * 100;
-        const Y=this.getComp(2) * 100;
-        const Z=this.getComp(3) * 100;
+        const X = this.getComp(1) * 100;
+        const Y = this.getComp(2) * 100;
+        const Z = this.getComp(3) * 100;
     
-        const xr = X/Xn;
-        const yr = Y/Yn;
-        const zr = Z/Zn;
+        const xr = X / Xn;
+        const yr = Y / Yn;
+        const zr = Z / Zn;
 
         const L = 116*f(yr) - 16;
         const a = 500*(f(xr) - f(yr));
