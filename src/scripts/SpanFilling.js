392e38e3e6fa79abc1ed748d69d485bf1da2aa3b
ToxykAuBleu <foissac.mathieu@gmail.com>
2023-12-20 15:23:53 +0100

    Debug en cours: [~] Transformation de calque en grille pour les tests. [~] Correction de checkIfInside: mauvaises conditions.

--- src/scripts/SpanFilling.js
+++ src/scripts/SpanFilling.js
@@ -23,27 +23,25 @@ function baguetteMagique(coords, tolerance, grille) {
     let coordY = coords.getY();
     // Conversion du pixel sélectionné en L*a*b*.
     let pixelOrigine = grille.getPixelAt(coordX, coordY);
-    console.log("RGB: ", pixelOrigine);
     let couleurRGB = new RGB(pixelOrigine.getColor().getComp(1), pixelOrigine.getColor().getComp(2), pixelOrigine.getColor().getComp(3));
-    let couleurXYZ = couleurRGB.RGBversXYZ();
-    let couleurLab = couleurXYZ.XYZversLab();
+    let couleurLab = couleurRGB.RGBversXYZ().XYZversLab();
     pixelOrigine.setColor(couleurLab);
-    console.log("Lab: ",pixelOrigine);
 
     let fileTraitement = [];
     fileTraitement.push(coords);
 
     // coordX, coordY, tolerance, grille, pixelOrigine, fileTraitement >> Effectuer la recherche >> grille
+    spanFilling(tolerance, grille, fileTraitement, pixelOrigine);
 }
 
 /**
  * Applique l'algorithme de SpanFilling sur le calque/grille.
  * @param {Float32Array} tolerance Représente la tolérance de remplissage.
- * @param {Calque} calque Représente le calque sur lequel on travaille.
+ * @param {Grille} grille Représente la grille sur lequel on travaille.
  * @param {Array<Coordonnees>} fileTraitement Représente la file de traitement contenant des Coordonnées des Pixels.
  * @param {Pixel} pixelOrigine Représente le Pixel d'origine (pixel sélectionné par l'utilisateur).
  */
-function spanFilling(tolerance, calque, fileTraitement, pixelOrigine) {
+function spanFilling(tolerance, grille, fileTraitement, pixelOrigine) {
     while (true) {
         // Vérification conditions d'arrêt.
         if (fileTraitement.length === 0) {
@@ -60,40 +58,46 @@ function spanFilling(tolerance, calque, fileTraitement, pixelOrigine) {
         let partieDroiteX = coordsTraitement.getX();
 
         // Traitement de la partie gauche du pixel courant. >> partieGaucheX, calque
-        while (checkIfInside(calque, partieGaucheX - 1, yFixe) 
-        && checkTolerance(calque, tolerance, partieGaucheX - 1, yFixe, pixelOrigine)) 
-        {
+        while (checkIfInside(grille, partieGaucheX - 1, yFixe)) {
+            if (!checkTolerance(grille, tolerance, partieGaucheX - 1, yFixe, pixelOrigine)) break;
+        
             // Sélection du pixel courant, puis pixel suivant.
-            calque.getGrille().getPixelAt(partieGaucheX - 1, yFixe).setSelected(true);
+            grille.getPixelAt(partieGaucheX - 1, yFixe).setSelected(true);
             partieGaucheX--;
         }
 
         // Traitement de la partie droite du pixel courant. >> partieDroiteX, calque
-        while (checkIfInside(calque, partieDroiteX, yFixe) 
-        && checkTolerance(calque, tolerance, partieDroiteX, yFixe, pixelOrigine)) 
-        {
+        while (checkIfInside(grille, partieDroiteX, yFixe)) {
+            if (!checkTolerance(grille, tolerance, partieDroiteX, yFixe, pixelOrigine)) break;
+
             // Sélection du pixel courant, puis pixel suivant.
-            calque.getGrille().getPixelAt(partieDroiteX, yFixe).setSelected(true);
+            grille.getPixelAt(partieDroiteX, yFixe).setSelected(true);
             partieDroiteX++;
         }
 
+        setTimeout(() => {}, 1000);
+        console.log(coordsTraitement);
         // partieGaucheX, partieDroiteX, yFixe, fileTraitement >> Scan dans les lignes du dessus et du dessous >> fileTraitement
-        scanLine(partieGaucheX, partieDroiteX - 1, yFixe + 1, fileTraitement)
-        scanLine(partieGaucheX, partieDroiteX - 1, yFixe - 1, fileTraitement)
+        scanLine(grille, pixelOrigine, partieGaucheX, partieDroiteX - 1, yFixe + 1, fileTraitement)
+        scanLine(grille, pixelOrigine, partieGaucheX, partieDroiteX - 1, yFixe - 1, fileTraitement)
     }
+    console.log("TERMINE !!!!");
+    return grille;
 }
 
 /**
  * Permet de trouver des nouveaux points à traiter pour l'algo de SpanFilling.
+ * @param {Grille} grille Représente la grille sur lequel on travaille.
+ * @param {Pixel} pixelOrigine Représente le Pixel d'origine (pixel sélectionné par l'utilisateur).
  * @param {Number} partieGaucheX Représente la coordonnée en x du pixel le plus à gauche de la ligne.
  * @param {Number} partieDroiteX Représente la coordonnée en x du pixel le plus à droite de la ligne.
  * @param {Number} y Représente la coordonnée en y de la ligne.
  * @param {Array<Coordonnees>} fileTraitement Représente la file de traitement contenant des Coordonnées des Pixels.
  */
-function scanLine(partieGaucheX, partieDroiteX, y, fileTraitement) {
+function scanLine(grille, pixelOrigine, partieGaucheX, partieDroiteX, y, fileTraitement) {
     let x = partieGaucheX;
     while (x <= partieDroiteX) {
-        if (checkIfInside(calque, x, y) && checkTolerance(calque, tolerance, x, y, pixelOrigine)) {
+        if (checkIfInside(grille, x, y) && checkTolerance(grille, tolerance, x, y, pixelOrigine)) {
             fileTraitement.push(new Coordonnees(x, y));
             break;
         }
@@ -103,16 +107,16 @@ function scanLine(partieGaucheX, partieDroiteX, y, fileTraitement) {
 
 /**
  * Permet de vérifier si le pixel au coordonnées (x, y) est dans les limites du calque.
- * @param {Calque} calque Représente le calque sur lequel on travaille.
+ * @param {Grille} grille Représente la grille sur lequel on travaille.
  * @param {Number} x Représente la coordonnée en x du pixel.
  * @param {Number} y Représente la coordonnée en y du pixel.
  * @returns {Boolean} true si le pixel est dans les limites du calque, false sinon.
  */
-function checkIfInside(calque, x, y) {
-    let largeur = calque.getGrille().getLargeur();
-    if (largeur <= x <= largeur + x) {
-        let hauteur = calque.getGrille().getHauteur();
-        if (hauteur <= y <= hauteur + y) {
+function checkIfInside(grille, x, y) {
+    let largeur = grille.getLargeur();
+    if ((0 <= x) && (x < largeur)) {
+        let hauteur = grille.getHauteur();
+        if ((0 <= y) && (y < hauteur)) {
             return true;
         } else {
             return false;
@@ -124,20 +128,21 @@ function checkIfInside(calque, x, y) {
 
 /**
  * Permet de vérifier si le pixel au coordonnées (x, y) doit être sélectionné.
- * @param {Calque} calque Représente le calque sur lequel on travaille.
+ * @param {Grille} grille Représente la grille sur lequel on travaille.
  * @param {Float32Array} tolerance Représente la tolérance.
  * @param {Number} x Représente la coordonnée en x du pixel.
  * @param {Number} y Représente la coordonnée en y du pixel.
  * @param {Pixel} pixelOrigine Représente le Pixel d'origine (pixel sélectionné par l'utilisateur).
  * @returns {Boolean} true si le pixel doit être sélectionné, false sinon.
  */
-function checkTolerance(calque, tolerance, x, y, pixelOrigine) {
+function checkTolerance(grille, tolerance, x, y, pixelOrigine) {
     // calque, x, y >> Récupération du pixel en coordonnées x, y et transformation en L*a*b*. >> couleurLab
-    let pixelCourant = calque.getGrille().getPixelAt(x, y);
-    let couleurRGB = new RGB(pixelCourant.getColor().getComp(1), pixelCourant.getColor().getComp(2), pixelCourant.getColor().getComp(3));
+    let pixelComp = grille.getPixelAt(x, y);
+    let couleurRGB = new RGB(pixelComp.getColor().getComp(1), pixelComp.getColor().getComp(2), pixelComp.getColor().getComp(3));
     let couleurLab = couleurRGB.RGBversXYZ().XYZversLab();
     
     let deltaE = couleurLab.calculDeltaE(pixelOrigine.getColor());    
+    console.log("checkTolerance: ", x, y, deltaE);
     if (deltaE <= tolerance) {
         return true;
     } else {
@@ -145,4 +150,4 @@ function checkTolerance(calque, tolerance, x, y, pixelOrigine) {
     }
 }
 
-export { baguetteMagique };
+export { baguetteMagique, checkIfInside };
