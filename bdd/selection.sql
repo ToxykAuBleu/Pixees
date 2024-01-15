7f2cb4b4f37b7a079832a04c7262d5e7be2ed245
Mattis Pingard <mattis.pingard@gmail.com>
2024-01-15 16:29:02 +0100

    [+] Ajout d'alias

--- bdd/selection.sql
+++ bdd/selection.sql
@@ -13,23 +13,23 @@ FROM Signalement
 WHERE idUtilisateur = 3;
 
 -- 4. Afficher le nombre total de publication
-SELECT COUNT(idProjet) FROM Editer
+SELECT COUNT(idProjet) NbProjet FROM Editer
 WHERE idUtilisateur = 1;
 
 -- 5. Afficher le nombre total de like
-SELECT SUM(idUtilisateur) FROM AimerPub
+SELECT COUNT(idUtilisateur) NbLike FROM AimerPub
 WHERE idPublication = 1;
 
 -- 6. Combien d'adeptes suit un utilisateur ?
-SELECT SUM(idUtilisateur) FROM SuivreArtiste
+SELECT SUM(idUtilisateur) SumUtilisateur FROM SuivreArtiste
 WHERE idUtilisateur_1 = 3;
 
 -- 7. Afficher le nombre de personne suivis
-SELECT SUM(idUtilisateur_1) FROM SuivreArtiste
+SELECT SUM(idUtilisateur_1) SumUtilisateur FROM SuivreArtiste
 WHERE idUtilisateur = 1;
 
 -- 8. Afficher le nombre de commentaire sur une publication
-SELECT COUNT(idCommentaire) FROM Commentaire
+SELECT COUNT(idCommentaire) NbCommentaire FROM Commentaire
 WHERE idPublication = 1;
 
 -- 10. Afficher les publications ayant eu le plus de likes dans les dernières 24 heures
