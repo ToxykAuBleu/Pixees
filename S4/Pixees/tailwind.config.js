3a3ed5e67bc060ec6f1201ec4e462653c5dd924a
Mattis Pingard <mattis.pingard@gmail.com>
2024-01-22 14:47:53 +0100

    [+] Ajout de tailwind

---
+++ S4/Pixees/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      title: ["Pixelify Sans", "sans-serif"],
+      body: ["PT Sans", "sans-serif"],
+    },
+  },
+  plugins: [],
+}
+
