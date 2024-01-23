0ef0e87fbb0d916b656217772ca25c1e748e7a36
Mattis Pingard <mattis.pingard@gmail.com>
2024-01-23 19:11:38 +0100

    [+] Ajout des icônes pour les outils

--- src/app/pages/editeur/outil/outil.component.ts
+++ src/app/pages/editeur/outil/outil.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPencil, faEraser, faVectorSquare, faFillDrip, faEyeDropper, faWandMagic, faShapes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-outil',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './outil.component.html',
   styleUrl: './outil.component.scss'
 })
 export class OutilComponent {
-
+  faPencil = faPencil;
+  faEraser = faEraser;
+  faVectorSquare = faVectorSquare;
+  faFillDrip = faFillDrip;
+  faEyeDropper = faEyeDropper;
+  faWandMagic = faWandMagic;
+  faShapes = faShapes;
+  constructor() { }
 }
