bd80ee6c6c4471b254f5415a6fbe673bfd12d450
neoelduayen <nelduayen@univ-pau.fr>
2024-01-24 17:37:57 +0100

    [+] Création et placement des éléments de la page compte

--- src/app/pages/compte/compte.component.ts
+++ src/app/pages/compte/compte.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-compte',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './compte.component.html',
   styleUrl: './compte.component.scss'
 })
 export class CompteComponent {
-
+  faArrowLeft = faArrowLeft;
+  constructor(private router: Router) { }
+  goToConnexion() {
+    this.router.navigate(['/', 'connexion']);
+  }
+  goToHome() {
+    this.router.navigate(['/', 'home']);
+  }
 }
