ee0de2967e18c8ba94d75b9ff5b764680c1cdeeb
ToxykAuBleu <foissac.mathieu@gmail.com>
2024-01-25 12:15:16 +0100

    [+] Début de la page Projet.

--- src/app/pages/projet/projet.component.ts
+++ src/app/pages/projet/projet.component.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFileCirclePlus, faFileImport, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-projet',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './projet.component.html',
   styleUrl: './projet.component.scss'
 })
 export class ProjetComponent {
+  faFileCirclePlus = faFileCirclePlus;
+  faFileImport = faFileImport;
+  faXmark = faXmark;
+
   constructor(private router: Router) {};
-  goToEditeur() {
+  newProject() {
     this.router.navigate(['/', 'editeur']);
+  }
+
+  importProject() {
+    console.log("Import projet");
   }
 }
