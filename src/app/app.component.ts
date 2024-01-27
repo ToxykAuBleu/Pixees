81e06bad039035224e9c88b6cbfd5014c324cbd7
Mattis Pingard <mattis.pingard@gmail.com>
2024-01-27 16:06:18 +0100

    [+] Ajout de la navbar en mode éditeur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,8 @@ import { ProfilComponent } from './pages/profil/profil.component';
 import { ProjetComponent } from './pages/projet/projet.component';
 import { NotificationsComponent } from './pages/notifications/notifications.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faHouse, faPencil , faPaperPlane, faBell, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { faCircleUser } from '@fortawesome/free-regular-svg-icons';
+import { faHouse, faPencil , faPaperPlane, faBell, faMagnifyingGlass, faDownload, faFloppyDisk, faFileImport, faRotateLeft, faRotateRight } from '@fortawesome/free-solid-svg-icons';
+import { faCircleUser, faXmarkCircle } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -27,6 +27,7 @@ import { faCircleUser } from '@fortawesome/free-regular-svg-icons';
 })
 
 export class AppComponent {
+  // Icone de la barre de navigation côté réseau social
   faHouse = faHouse;
   faPencil = faPencil;
   faPaperPlane = faPaperPlane;
@@ -34,32 +35,48 @@ export class AppComponent {
   faCircleUser = faCircleUser;
   faMagnifyingGlass = faMagnifyingGlass;
 
+  // Icone de la barre de navigation côté éditeur
+  faDownload = faDownload;
+  faFloppyDisk = faFloppyDisk;
+  faFileImport = faFileImport;
+  faRotateLeft = faRotateLeft;
+  faRotateRight = faRotateRight;
+  faXmarkCircle = faXmarkCircle;
+
   title = 'Pixees';
   public couleur = "couleurAccueil";
+  isInEditor: boolean = false;
+
   constructor(private router: Router) {};
   goToHome() {
     this.router.navigate(['/', 'home']);
     this.couleur = "couleurAccueil";
+    this.isInEditor = false;
   }
   goToProjet() {
     this.router.navigate(['/', 'projet']);
-    this.couleur = "couleurUtilisateur";
+    this.couleur = "couleurCreation";
+    this.isInEditor = true;
   }
   goToNotification() {
     this.router.navigate(['/', 'notifications']);
     this.couleur = "couleurProfil";
+    this.isInEditor = false;
   }
   goToAccount() {
     this.router.navigate(['/', 'compte']);
     this.couleur = "couleurProfil";
+    this.isInEditor = false;
   }
   goToDiscussion() {
     this.router.navigate(['/', 'discussion']);
     this.couleur = "couleurDiscussion";
+    this.isInEditor = false;
   }
   goToConnexion() {
     this.router.navigate(['/', 'connexion']);
     this.couleur = "couleurProfil";
+    this.isInEditor = false;
   }
 }
 
