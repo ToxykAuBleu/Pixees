8ef8987b17cbd3a7705aef8eb08cc11d5e7863b6
Mattis Pingard <mattis.pingard@gmail.com>
2024-01-27 18:46:23 +0100

    [+] Ajout de l'affichage des calques

--- src/app/pages/editeur/calque/calque.component.ts
+++ src/app/pages/editeur/calque/calque.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCirclePlus, faTrashCan, faArrowUp, faArrowDown, faCopy } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-calque',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './calque.component.html',
   styleUrl: './calque.component.scss'
 })
 export class CalqueComponent {
-
+  faCirclePlus = faCirclePlus;
+  faTrashCan = faTrashCan;
+  faArrowUp = faArrowUp;
+  faArrowDown = faArrowDown;
+  faCopy = faCopy;
 }
