dcd5a654c970be72dc20c67bfc9c3cbca8d7893d
neoelduayen <nelduayen@univ-pau.fr>
2024-03-05 16:53:21 +0100

    [+] Ajout de style et d'ordre sur la page vitrine

--- src/app/pages/vitrine/vitrine.component.ts
+++ src/app/pages/vitrine/vitrine.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-vitrine',
-  standalone: true,
-  imports: [],
-  templateUrl: './vitrine.component.html',
-  styleUrl: './vitrine.component.scss'
+    selector: 'app-vitrine',
+    standalone: true,
+    templateUrl: './vitrine.component.html',
+    styleUrl: './vitrine.component.scss',
+    imports: []
 })
 export class VitrineComponent {
   constructor(private router: Router) {
