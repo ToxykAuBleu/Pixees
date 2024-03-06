5635c33cc7caabc2b303df57b4972285f725665e
neoelduayen <nelduayen@univ-pau.fr>
2024-03-06 14:34:17 +0100

    [+] Finalisation du composant CarouselVitrine

--- src/app/pages/vitrine/carousel-vitrine/carousel-vitrine.component.ts
+++ src/app/pages/vitrine/carousel-vitrine/carousel-vitrine.component.ts
@@ -11,12 +11,12 @@ import { CommonModule } from '@angular/common';
 })
 export class CarouselVitrineComponent {
   slides = [
-    {img: "assets/img/editeur.png"},
-    {img: "assets/img/admin-utilisateur.png"},
-    {img: "assets/img/accueil.png"},
-    {img: "assets/img/admin-admin.png"},
-    {img: "assets/img/profil.png"},
-    {img: "assets/img/discussion.png"},
+    {img: "assets/img/editeur.png", caption: "Editeur de contenu"},
+    {img: "assets/img/admin-utilisateur.png", caption: "Gestion des utilisateurs"},
+    {img: "assets/img/accueil.png", caption: "Accueil"},
+    {img: "assets/img/admin-admin.png", caption: "Gestion des administrateurs"},
+    {img: "assets/img/profil.png", caption: "Profil"},
+    {img: "assets/img/discussion.png", caption: "Discussion"},
   ];
 
   slideConfig = {
@@ -24,8 +24,8 @@ export class CarouselVitrineComponent {
     "slidesToScroll": 1,
     "infinite": true,
     "autoplay": true,
-    "autoplaySpeed": 8000,
-    "arrows": true,
+    "autoplaySpeed": 5000,
+    "arrows": false,
     "touchMove": false,
     "arrowLeft": "<div class='slick-prev'><i class='fa fa-chevron-left'></i></div>",
     "arrowRight": "<div class='slick-next'><i class='fa fa-chevron-right'></i></div>",
@@ -37,8 +37,7 @@ export class CarouselVitrineComponent {
           "slidesToScroll": 1,
           "infinite": true,
           "autoplay": true,
-          "autoplaySpeed": 8000,
-          "arrows": true,
+          "autoplaySpeed": 5000,
         }
       },
       {
@@ -48,8 +47,7 @@ export class CarouselVitrineComponent {
           "slidesToScroll": 1,
           "infinite": true,
           "autoplay": true,
-          "autoplaySpeed": 8000,
-          "arrows": true,
+          "autoplaySpeed": 5000,
         }
       },
       {
@@ -59,45 +57,9 @@ export class CarouselVitrineComponent {
           "slidesToScroll": 1,
           "infinite": true,
           "autoplay": true,
-          "autoplaySpeed": 8000,
-          "arrows": true,
+          "autoplaySpeed": 5000,
         }
       }
     ]
   };
-  public description:String = "placeholder description";
-  public i:number = 0;
-
-  changeDescription(i:number){
-    switch(i){
-      case 0:
-        this.description = "Editeur de contenu";
-        break;
-      case 1:
-        this.description = "Gestion des utilisateurs";
-        break;
-      case 2:
-        this.description = "Accueil";
-        break;
-      case 3:
-        this.description = "Gestion des administrateurs";
-        break;
-      case 4:
-        this.description = "Profil";
-        break;
-      case 5:
-        this.description = "Discussion";
-        break;
-      default:
-        this.description = "placeholder description";
-        break;
-    };
-    if (i <= 5) {
-      i++;
-    }
-    else {
-      i = 0;
-    }
-    setTimeout('changeDescription',8000)
-  }
 }
