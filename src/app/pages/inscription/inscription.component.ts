45a63aa1a54c5c31e1ec8756239ccc13ee0b716c
Adrien Hiribarren Touya <adrien64250@yahoo.com>
2024-03-06 09:29:35 +0100

    [+] Vérification pseudo fonctionnelle, à styliser [~] Relocalisation du fichier register.php pour plus de logique

--- src/app/pages/inscription/inscription.component.ts
+++ src/app/pages/inscription/inscription.component.ts
@@ -1,16 +1,17 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { environment } from '../../../../environment';
 import { AppService } from '../../app.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inscription',
   standalone: true,
-  imports: [FontAwesomeModule, ReactiveFormsModule],
+  imports: [FontAwesomeModule, ReactiveFormsModule, CommonModule],
   templateUrl: './inscription.component.html',
   styleUrl: './inscription.component.scss'
 })
@@ -18,11 +19,15 @@ export class InscriptionComponent {
   faArrowLeft = faArrowLeft;
 
   signUpForm: FormGroup = this.formBuilder.group({
-    pseudo: ['', [Validators.required, Validators.min(3), Validators.max(50)]],
-    email: ['', [Validators.required, Validators.email, Validators.max(50)]],
-    mdp: ['', [Validators.required, Validators.min(8), Validators.max(50)]],
-    confirmation: ['', [Validators.required, Validators.min(8), Validators.max(50)]]
+    pseudo: ['', [Validators.required, Validators.maxLength(50)]],
+    email: ['', [Validators.required, Validators.email, Validators.maxLength(50)]],
+    mdp: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(50)]],
+    confirmation: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(50)]]
   });
+
+  get pseudo(): FormControl { return this.signUpForm.get('pseudo') as FormControl; }
+  get email(): FormControl { return this.signUpForm.get('email') as FormControl; }
+  get mdp(): FormControl { return this.signUpForm.get('mdp') as FormControl; }
 
   constructor(private formBuilder: FormBuilder,
     private http: HttpClient,
@@ -43,7 +48,7 @@ export class InscriptionComponent {
       withCredentials: true
     };
 
-    this.http.post(`${environment.apiLink}/project/register.php`, this.signUpForm.value, httpOptions)
+    this.http.post(`${environment.apiLink}/user/register.php`, this.signUpForm.value, httpOptions)
       .subscribe({
         next: (res) => {
           if (res.valueOf().hasOwnProperty('error')) {
