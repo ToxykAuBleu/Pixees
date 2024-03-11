a3afd93e04a84499b8e97c9cad2fb98f234223df
ToxykAuBleu <foissac.mathieu@gmail.com>
2024-03-11 11:25:51 +0100

    [+] Création du backend pour charger un projet.

---
+++ backend/api/project/list.php
@@ -0,0 +1,41 @@
+<?php
+    session_start();
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Methods: *");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers");
+
+    if ( !isset($_SESSION["id"]) ) {
+        echo json_encode(array("error" => "Vous n'êtes pas connecté"));
+        exit();
+    }
+
+    // Connexion à la base de données
+    $ini = parse_ini_file("../config.ini", true);
+    try {
+        $link = mysqli_connect($ini["PROJET"]["Adresse"], $ini["PROJET"]["Utilisateur"], $ini["PROJET"]["MotPasse"], $ini["PROJET"]["Database"]);
+    } catch (Exception $e) {
+        echo json_encode(array("error" => "Erreur de connexion à la base de données"));
+        exit;
+    }
+
+    $projects = [];
+    // Récupération des métadonnées du projet dans la bdd.
+    try {
+        $query = "SELECT E.idUtilisateur utilisateur, P.idProjet projet, nom, hauteurToile, largeurToile, dateCreation, dateModif FROM ".$ini["PROJET"]["Table_Project"]." P JOIN Editer E ON P.idProjet = E.idProjet WHERE idUtilisateur = ".$_SESSION["id"].";";
+        $result = mysqli_query($link, $query);
+        if ( !$result ) {
+            echo json_encode(array("error" => "Erreur de récupération des projets"));
+            exit;
+        }
+    
+        while ($project = $result->fetch_assoc()) {
+            $projects[] = $project;
+        }
+    } catch (Exception $e) {
+        echo json_encode(array("error" => $e));
+        exit;
+    }
+
+    echo json_encode($projects);
+?>
