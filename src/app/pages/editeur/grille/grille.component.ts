a9c4df8ff5d1f7229bc8e7eaa8337976a5bca575
Mattis Pingard <mattis.pingard@gmail.com>
2024-03-13 18:58:47 +0100

    [~] Correction du problème de canvas pour le chargement d'un projet

--- src/app/pages/editeur/grille/grille.component.ts
+++ src/app/pages/editeur/grille/grille.component.ts
@@ -81,6 +81,25 @@ export class GrilleComponent implements AfterViewInit, OnInit, OnDestroy, OnChan
     // this.ctx = this.canvas?.nativeElement.getContext('2d');
     this.gridCtx = this.gridCanvas?.nativeElement.getContext('2d');
     this.canvases!.changes.subscribe(() => {
+      if (this.isLoadingProject) {
+        for (const layer of this.layers) {
+          const grille = layer.getGrille();
+          const pos = layer.getPosition();
+          // FIXME: this.canvases totalement vide !!!! 
+          const ctx = this.canvases!.toArray()[pos].nativeElement.getContext('2d') as CanvasRenderingContext2D;
+          for (let x = 0; x < this.largeur; x++) {
+            for (let y = 0; y < this.hauteur; y++) {
+              const color = (grille.getPixelAt(x, y).getColor() as RGB).RGBversHexa().slice(1);
+              console.log(color);
+              const formattedColor = `rgba(${parseInt(color.slice(0,2), 16)}, ${parseInt(color.slice(2,4), 16)}, ${parseInt(color.slice(4,6), 16)}, ${parseInt(color.slice(6,8), 16)})`;
+              ctx.fillStyle = formattedColor;
+              ctx.fillRect(x, y, 1, 1);
+            }
+          }
+          console.log(ctx);
+        }
+        console.log("Chargement terminé !");
+      }
       this.ctx = this.canvases!.toArray()[this.selectedLayerIndex].nativeElement.getContext('2d');
     });
     // Récupération de la hauteur et de la largeur du canvas
@@ -126,28 +145,6 @@ export class GrilleComponent implements AfterViewInit, OnInit, OnDestroy, OnChan
     if (changes['selectedLayerIndex']) {
       this.ctx = this.canvases!.toArray()[this.selectedLayerIndex].nativeElement.getContext('2d');
     } 
-    // Chargement des calques
-    if (changes['isLoadingProject'] && (changes['isLoadingProject'].previousValue === false && changes['isLoadingProject'].currentValue === true)) {
-      console.log("Chargement des layers !");
-      for (const layer of this.layers) {
-        const grille = layer.getGrille();
-        const pos = layer.getPosition();
-        // FIXME: this.canvases totalement vide !!!! 
-        console.log("Liste canvases: ", this.canvases!.toArray());
-        console.log("Ctx: ", this.canvases!.toArray()[pos]);
-        const ctx = this.canvases!.toArray()[pos].nativeElement.getContext('2d') as CanvasRenderingContext2D;
-        for (let x = 0; x < this.largeur; x++) {
-          for (let y = 0; y < this.hauteur; y++) {
-            const color = (grille.getPixelAt(x, y).getColor() as RGB).RGBversHexa().slice(1);
-            const formattedColor = `rgba(${parseInt(color.slice(0,2), 16)}, ${parseInt(color.slice(2,4), 16)}, ${parseInt(color.slice(4,6), 16)}, ${parseInt(color.slice(6,8), 16)})`;
-            console.log(formattedColor);
-            ctx.fillStyle = formattedColor;
-            ctx.fillRect(x, y, 1, 1);
-          }
-        }
-      }
-      console.log("Chargement terminé !");
-    }
   }
 
   getMousePos(canvas: HTMLCanvasElement, evt: MouseEvent) {
