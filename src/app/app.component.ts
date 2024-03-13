18027e544f31a46fc7e148d134940326178d7c9e
ToxykAuBleu <foissac.mathieu@gmail.com>
2024-03-13 20:46:01 +0100

    [~] Bug fix: l'icone GitHub apparaissait même quand il ne fallait pas.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -58,6 +58,7 @@ export class AppComponent implements OnInit, OnDestroy {
   gitRepoInfo = gitRepoInfo;
   gitLastModifDate = new Date(gitRepoInfo.date).toLocaleString();
   showGitInfo = false;
+  isAppProduction = environment.production;
 
   title = 'Pixees';
 
